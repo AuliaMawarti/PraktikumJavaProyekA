e2ea4ee8e33d2017879e7c42aa3d242a2a676516
camelion <mikroandi211@gmail.com>
2022-07-01 11:36:27 +0800

    Menampilkan relasi data one to many dan penggunaan JComboBox dinamis

---
+++ src/main/java/frame/KecamatanViewFrame.java
@@ -0,0 +1,161 @@
+package frame;
+
+import helpers.Koneksi;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.*;
+
+public class KecamatanViewFrame extends JFrame {
+    private JPanel cariPanel;
+    private JTextField cariTextField;
+    private JButton cariButton;
+    private JScrollPane viewScrollPanel;
+    private JTable viewTable;
+    private JPanel butttonPanel;
+    private JButton tambahButton;
+    private JButton ubahButton;
+    private JButton hapusButton;
+    private JButton batalButton;
+    private JButton cetakButton;
+    private JButton tutupButton;
+    private JPanel mainPanel;
+
+    public KecamatanViewFrame() {
+        tutupButton.addActionListener(e -> {dispose();});
+        batalButton.addActionListener(e -> {isiTable();});
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowActivated(WindowEvent e){
+                isiTable();
+            }
+        });
+
+        cariButton.addActionListener(e -> {
+
+            if (cariTextField.getText().equals("")) {
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Isi Kata Kunci Pencarian",
+                        "Validasi Kata Kunci kosong",
+                        JOptionPane.WARNING_MESSAGE
+                );
+                return;
+            }
+
+            Connection c = Koneksi.getConnection();
+            String keyword = "%" + cariTextField.getText() + "%";
+            String searchSQL = "SELECT K.*, B.nama AS nama_kabupaten FROM kecamatan K LEFT JOIN kabupaten B ON K.kabupaten_id = B.id WHERE K.nama like ? OR B.nama like ?";
+            try {
+                PreparedStatement ps = c.prepareStatement(searchSQL);
+                ps.setString(1, keyword);
+                ps.setString(2, keyword);
+                ResultSet rs = ps.executeQuery();
+                DefaultTableModel dtm = (DefaultTableModel) viewTable.getModel();
+                dtm.setRowCount(0);
+                Object[] row = new Object[3];
+                while (rs.next()) {
+                    row[0] = rs.getInt("id");
+                    row[1] = rs.getString("nama");
+                    row[2] = rs.getString("nama_kabupaten");
+                    dtm.addRow(row);
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+//perintah hapus button
+        hapusButton.addActionListener(e -> {
+            int barisTerpilih = viewTable.getSelectedRow();
+            if (barisTerpilih < 0) {
+                JOptionPane.showMessageDialog(null, "Pilih data dulu");
+                return;
+            }
+            int pilihan = JOptionPane.showConfirmDialog(
+                    null,
+                    "Yakin mau hapus?",
+                    "Konfirmasi Hapus",
+                    JOptionPane.YES_NO_OPTION
+            );
+
+            if (pilihan == 0 ) {
+                TableModel tm = viewTable.getModel();
+                int id = Integer.parseInt(tm.getValueAt(barisTerpilih,0).toString());
+                Connection c = Koneksi.getConnection();
+                String deleteSQL = "DELETE FROM kecamatan WHERE id = ?";
+                try {
+                    PreparedStatement ps = c.prepareStatement(deleteSQL);
+                    ps.setInt(1, id);
+                    ps.executeUpdate();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        //end perintah hapus button
+
+        tambahButton.addActionListener(e -> {
+            KecamatanInputFrame inputFrame = new KecamatanInputFrame();
+            inputFrame.setVisible(true);
+        });
+
+        ubahButton.addActionListener(e -> {
+            int barisTerpilih = viewTable.getSelectedRow();
+            if (barisTerpilih < 0) {
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Pilih data dulu"
+                );
+                return;
+            }
+
+            TableModel tm = viewTable.getModel();
+            int id = Integer.parseInt(tm.getValueAt(barisTerpilih, 0).toString());
+            KecamatanInputFrame inputFrame = new KecamatanInputFrame();
+            inputFrame.setId(id);
+            inputFrame.isiKomponen();
+            inputFrame.setVisible(true);
+        });
+
+        isiTable();
+        init();
+
+    }
+
+    public void init() {
+        setContentPane(mainPanel);
+        setTitle("Data Kabupaten");
+        pack();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public void isiTable() {
+        Connection c = Koneksi.getConnection();
+        String selectSQL = "SELECT K.*, B.nama AS nama_kabupaten FROM kecamatan K LEFT JOIN kabupaten B ON K.kabupaten_id=B.id";
+        try {
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery(selectSQL);
+            String header[] = {"Id", "Nama Kecamatan", "Nama Kabupaten"};
+            DefaultTableModel dtm = new DefaultTableModel(header, 0);
+            viewTable.setModel(dtm);
+
+            viewTable.removeColumn(viewTable.getColumnModel().getColumn(0));
+
+            Object[] row = new Object[3];
+            while (rs.next()) {
+                row[0] = rs.getInt("id");
+                row[1] = rs.getString("nama");
+                row[2] = rs.getString("nama_kabupaten");
+                dtm.addRow(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
